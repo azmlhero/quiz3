b87f839a4eb0db4937a07f636c96dfdf29a5dd9d
ammar <azml1@gmail.com>
2021-06-28 10:05:41 +0500

    a

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,6 @@
 const jwt= require("jsonwebtoken");
 const config = require("config");
-const{User}=require("../models/user")
+const User =require("../models/user")
 async function  auth (req,res,next){
     let token= req.header("x-auth-token");
     if(!token) return res.status(400).send("token Not provided");
